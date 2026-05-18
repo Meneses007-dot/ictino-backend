0df1e03500885aa11a250106111d9e4b76bcb6ff
Juan Manuel Meneses Daza <jmeneses612@gmail.com>
2026-05-17 19:23:09 -0500

    fix: unificar url de produccion y correccion dinamica de bboxes en el canvas

--- app.py
+++ app.py
@@ -241,6 +241,5 @@ with app.app_context():
 if __name__ == '__main__':
     # Lee el puerto dinámico de Render, si no existe (local), usa el 5000
     port = int(os.environ.get('PORT', 5000))
-    
     # En producción, debug debe ser False para evitar vulnerabilidades
     app.run(host="0.0.0.0", port=port, debug=False)
